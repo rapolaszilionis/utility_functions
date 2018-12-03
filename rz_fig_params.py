47e24a3b114639f2833533cfdbbb9f6770d88f7a
rapolas.zilionis <rapolas.zilionis@gmail.com>
2018-12-03 10:15:50 +0200

    renamed the function file, added figure parameters

---
+++ rz_fig_params.py
@@ -0,0 +1,29 @@
+# my global plotting parameters for the entire notebook
+
+import matplotlib as mpl
+mylines = 0.15*2.82 # the number 2.82 is the difference
+					# between Illustrator 1 pt and python 1 pt.
+mpl.rcParams['axes.linewidth'] = mylines # default 1
+mpl.rcParams['ytick.direction'] = 'out' # default 'in'
+mpl.rcParams['xtick.direction'] = 'out' # default 'in'
+mpl.rcParams['xtick.major.size'] = 2 # default 4
+mpl.rcParams['ytick.major.size'] = 2 # default 4
+mpl.rcParams['xtick.major.width'] = mylines # default 0.5
+mpl.rcParams['ytick.major.width'] = mylines # default 0.5
+mpl.rcParams['grid.linewidth'] = mylines/1.5 # default 0.5
+mpl.rcParams['grid.color'] = '0.8' # default 'k'
+mpl.rcParams['grid.linestyle'] = 'solid'# default ':'
+mpl.rcParams['legend.frameon'] = False # default True
+
+# the one below is to increase the resolution of inline images
+mpl.rcParams['figure.dpi']= 150
+mpl.rc("savefig", dpi=150)
+
+# set fontsize and style:
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+# the two lines below will make fonts in pdfs readable
+mpl.rcParams['pdf.fonttype'] = 42
+mpl.rcParams['ps.fonttype'] = 42
+plt.rc('font', family = 'Myriad Pro',size = 6)
