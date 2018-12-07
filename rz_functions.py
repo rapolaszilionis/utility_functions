eb06f5510f0db87e300aa362a04df1b2c68b5277
rapolas.zilionis <rapolas.zilionis@gmail.com>
2018-12-07 12:58:17 +0200

    moved import statements to a separate .py file

--- rz_functions.py
+++ rz_functions.py
@@ -1,28 +1,6 @@
-# import statements
-
-import platform
-print("python version:",platform.python_version()) # prints python version
-
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-from matplotlib.colors import LinearSegmentedColormap
-
-import time
-import datetime
-import pandas as pd
-import numpy as np
-import scipy
-from scipy import stats
-from scipy import sparse
-import copy
-import sys,os
-
-import statsmodels.sandbox.stats.multicomp
-
-
-
 # Functions
+
+from rz_import_statements import *
 
 def oset(a_list):
     """given a list/1d-array, returns an ordered set (list)"""
---
+++ rz_import_statements.py
@@ -0,0 +1,21 @@
+# import statements
+
+import platform
+print("python version:",platform.python_version()) # prints python version
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib.colors import LinearSegmentedColormap
+
+import time
+import datetime
+import pandas as pd
+import numpy as np
+import scipy
+from scipy import stats
+from scipy import sparse
+import copy
+import sys,os
+
+import statsmodels.sandbox.stats.multicomp
